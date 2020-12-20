2cd5ace311afcffa6c73c45b10656a8020a47ce2
Rohit Singh <conankmails@gmail.com>
2020-12-20 17:55:53 +0530

    more functions of pywhatkit tested

--- Whatsapp_Automation(pywhatkit)/whatAutomation.py
+++ Whatsapp_Automation(pywhatkit)/whatAutomation.py
@@ -1,4 +1,6 @@
 import pywhatkit
+
+
 def inputFunc():
     ContactTo = input('Enter the whatsapp no to whome you want to send the message: ')
     message = input("Enter Yout message: ")
@@ -11,13 +13,28 @@ def inputFunc():
 def sendPersonAutomated(ContactTo,message,timeHour,timeMin):
     pywhatkit.sendwhatmsg(ContactTo,message,timeHour,timeMin,wait_time=10,print_waitTime=True)
 
+
 #This for info from wiki
 def WikiInfo(topic):
     pywhatkit.info(topic,lines=2)
 
 
+#This is for  converting any given image to ASCII art
+def asciiConv(image,img):
+    pywhatkit.image_to_ascii_art(image,f'{img}.txt')
+
+
+#This is for  converting text to handwritten font with color combination 0,0,0 in RGB form
+def handWrittenConv():
+    pywhatkit.text_to_handwriting('Hello I am Rohit Kumar',rgb=[0,0,0])
+
+
 if __name__ == "__main__":
     #ContactTo,message,timeHour,timeMin = inputFunc()
     #sendPersonAutomated(ContactTo,message,timeHour,timeMin)
-    WikiInfo('India')
     
+    #WikiInfo('India')
+
+    #asciiConv('guglani.png','guglani')
+
+    handWrittenConv()
