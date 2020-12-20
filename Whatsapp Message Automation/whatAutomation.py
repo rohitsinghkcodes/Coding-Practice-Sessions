27e3c96f5e330249d6db408f1481271be79e14b9
Rohit Singh <conankmails@gmail.com>
2020-12-20 15:18:23 +0530

    WhatsApp message seding automation

---
+++ Whatsapp Message Automation/whatAutomation.py
@@ -0,0 +1,17 @@
+import pywhatkit
+def inputFunc():
+    ContactTo = input('Enter the whatsapp no to whome you want to send the message: ')
+    message = input("Enter Yout message: ")
+    timeHour = int(input("Enter time of sending the message(hours): "))
+    timeMin = int(input("Enter time of sending the message(mins): "))
+
+    return ContactTo,message,timeHour,timeMin
+
+ 
+def sendAutomated(ContactTo,message,timeHour,timeMin):
+    pywhatkit.sendwhatmsg(ContactTo,message,timeHour,timeMin)
+
+if __name__ == "__main__":
+    ContactTo,message,timeHour,timeMin = inputFunc()
+    sendAutomated(ContactTo,message,timeHour,timeMin)
+    
