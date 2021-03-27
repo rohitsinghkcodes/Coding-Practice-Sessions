5de624a19d2dee3faa306f38ad98eb5a8d7207f3
Rohit Singh <conankmails@gmail.com>
2021-03-27 16:19:33 +0530

    commit on 27 march

---
+++ HackerRank/Time_Conversion.cpp
@@ -0,0 +1,20 @@
+//incomplete
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    int len=s.length();
+string hour = to_string(s[0]).append((s[1]));
+    // if(s[len-1]=='P' &&   )
+    // {
+    //     cout<<s;
+    // }
+    // else
+    // {
+
+    // }
+    cout<<hour;
+    return 0;
+}
