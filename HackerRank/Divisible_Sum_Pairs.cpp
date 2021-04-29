da59994da4e256167f6e95dfd7d88abb0a480ab2
Rohit Singh <conankmails@gmail.com>
2021-04-29 10:41:48 +0530

    some codes updated

---
+++ HackerRank/Divisible_Sum_Pairs.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string.h>
+#include<set>
+#include<cmath>
+#include<numeric>
+#define LL long long
+using namespace std;
+
+int divisibleSumPairs(int n, int k, vector<int> ar) {
+
+
+}
+
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    vector<int> v(n),v2(n);
+    for(int &it:v) cin>>it;
+    for(int &it:v)
+    {
+        if()
+    }
+    return 0;
+}
---
+++ HackerRank/hashmapCheck.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int &it:v) cin>>it;
+
+    map<int,int> hashmap;
+    for(auto i=v.begin(); i!=v.end();i++)
+    {
+        hashmap[*i]++;
+    }
+
+    int max = 0,val=0;
+    for(auto i=hashmap.begin(); i!= hashmap.end(); i++)
+        {
+            // cout<<i->first<<"->"<<i->second<<endl;
+            if(i->second>max)
+            {
+                max = i->second;
+                val = i->first;
+            }
+        }
+        cout<<val;
+}
